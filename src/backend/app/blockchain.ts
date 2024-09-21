5cb4fbbd282acc0402cc77326e0f8407fb068659
fareeza05 <fareezarahman123@gmail.com>
2024-09-21 18:24:18 -0400

    Added documentation + bug fixes

--- src/backend/app/blockchain.ts
+++ src/backend/app/blockchain.ts
@@ -1,7 +1,17 @@
-// Implementing Blockchain Class
+// Imports
 const { v4: uuidv4 } = require('uuid');
+import * as crypto from 'crypto';
 
 
+/**
+ * This is the Block class
+ * @constructor This intializes properties
+ * @index This is the position of the block in the blockchain
+ * @timestamp This is the time the block was created
+ * @data This is the actual data we want to store
+ * @previousHash This is the hash of the previous block to maintain chain
+ * @hash This is the hash of the current block
+ */
 class Block{
     constructor( 
         public index: number, 
@@ -13,7 +23,16 @@ class Block{
     ){}
 }
 
-// Blockchain Class
+/**
+ * This is the blockchain class
+ * @Properties Chain: An array that stores all the blocks in the blockchain
+ * @Constructor Initializes blockchain by creating genesis block
+ * @createGenesisBlock Creates first block with index 0, timestamp, data, and prev hash of 0
+ * @calculateHash Uses crypto library (built-in) to create a sha256 hash
+ * @addBlock Adds new block to blockchain
+ * @getChain Returns current state of blockchain, allowing user to see all blocks
+ * 
+ */
 class Blockchain {
     chain: Block[];
 
@@ -27,7 +46,7 @@ class Blockchain {
 
     private calculateHash(index: number, timestamp:Date, data:any, previousHash: string) : string {
         // What is happening here?
-        return crypto.createHash('sha256').update(index + timestamp + JSON.stringify(data) + previousHash).digest('hex');
+        return crypto.createHash('sha256').update(index.toString() + timestamp.toISOString() + JSON.stringify(data) + previousHash).digest('hex');
     }
 
     public addBlock(data: any) : void {
